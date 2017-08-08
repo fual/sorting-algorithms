8d06640c480f32a9daa399ddd21aff377fd44410
akhlebaev <akhlebaev@yamoney.ru>
2017-08-09 01:10:08 +0300

    fix index and READ.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sorting-algorithms
+
+- [Bubble sort](https://en.wikipedia.org/wiki/Bubble_sort)
+- [Insertion sort](https://en.wikipedia.org/wiki/Insertion_sort)
+- [Merge sort](https://en.wikipedia.org/wiki/Merge_sort)
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-Array.prototype.swap = function(index1, index2) {
+Array.prototype.swap = (index1, index2) => {
   const value1 = this[index1];
   const value2 = this[index2];
   this[index1] = value2;
   this[index2] = value1;
 };
 
-const bubbleSort = require('./bubble.js');
-const insertionSort = require('./insertion.js')
-const mergeSort = require('./merge.js')
+const bubble = require('./bubble.js');
+const insertion = require('./insertion.js');
+const merge = require('./merge.js');
 
-//console.log(bisection([1,3,2,8,7,6,5,4], (...item) => mergeArray(...item)));
-console.log(mergeSort([1,3,2,8,7,6,5,4]));
+module.exports = {
+  bubble,
+  insertion,
+  merge,
+};
